c9bbdc8399ad3c1cb042c5ddc1d95a71b3f00f2f
Nicolas C.S <117557162+MrZLeviatan@users.noreply.github.com>
2025-10-22 20:01:18 -0500

    Update

--- src/main/java/co/edu/uniquindio/config/WebSocketConfig.java
+++ src/main/java/co/edu/uniquindio/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws") // URL del endpoint WebSocket
-                .setAllowedOriginPatterns("*") // Permite todas las conexiones (puedes restringir por dominio)
+                .setAllowedOriginPatterns("https://pfs3-storeit.web.app") // Permite todas las conexiones (puedes restringir por dominio)
                 .withSockJS(); // Permite compatibilidad con navegadores que no soportan WebSocket nativo
     }
 
